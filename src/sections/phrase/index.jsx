ac3cefe6407c0b58c53cbdae2943ce29139eb1c7
josezanle <jose.rios.lm@gmail.com>
2024-07-28 17:40:26 -0300

    update

--- src/sections/phrase/index.jsx
+++ src/sections/phrase/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { YELLOW } from '@/theme'
+import { BLACK, YELLOW } from '@/theme'
 import { useTranslations } from 'next-intl'
 import Title from '@/components/text/title'
 
@@ -12,7 +12,7 @@ const Phrase = () => {
         <div className='container__phrase'>
             <span className='content'>
                 <div className="box__title">
-                    <Title value={translate("phrase.phraseTitle")} color={YELLOW} />
+                    <Title value={translate("phrase.phraseTitle")} color={BLACK} />
                 </div>
 
                 <p className='single_text'>{translate("phrase.phraseText1")} 
